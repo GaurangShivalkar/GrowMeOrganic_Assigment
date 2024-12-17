c1ed36b0c1cf9b0b29997794a20e476d4287e649
GaurangShivalkar <gaurangshivalkar@gmail.com>
2024-12-17 20:10:03 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ export default function App() {
     
       <BrowserRouter>
       <Routes>
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
    
