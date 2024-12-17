d039ca473055f315cea8cd9de4e5914e5670a7e7
GaurangShivalkar <gaurangshivalkar@gmail.com>
2024-12-17 20:26:22 +0530

    first commit

--- src/components/RowSelectionForm.tsx
+++ src/components/RowSelectionForm.tsx
@@ -17,7 +17,7 @@ const RowSelectionForm = ({ onSubmit }: RowSelectionFormProps) => {
   return (
     <form
       onSubmit={handleFormSubmit}
-      style={{ display: "flex", flexDirection: "column", gap: "0.5rem", padding: "1rem" }}
+      style={{ display: "flex", flexDirection: "column", padding: "1px" }}
     >
       <label htmlFor="numberInput" style={{ fontWeight: "bold" }}>
         Enter Number of Rows to Select
@@ -27,7 +27,7 @@ const RowSelectionForm = ({ onSubmit }: RowSelectionFormProps) => {
         id="numberInput"
         name="numberInput"
         placeholder="Enter a value"
-        style={{ padding: "0.5rem", border: "1px solid #ccc", borderRadius: "4px" }}
+        style={{ padding: "1px" }}
       />
       <Button type="submit" label="Submit" className="p-button-sm" />
     </form>
